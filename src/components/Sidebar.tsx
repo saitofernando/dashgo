c018a850d43f5583073e98a0cba72ef5a7607bf5
Fernando Saito <fernandosaito@hotmail.com.br>
2022-10-27 18:11:35 -0400

    adicionado mais menus ao sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,31 +6,46 @@ export function Sidebar() {
     <Box as="aside" w="64" mr="8">
        <Stack spacing="12" align="flex-start">
           <Box>
-            <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
+            <Text fontWeight="bold" color="gray.400" fontSize="small">ATENDIMENTO</Text>
             <Stack spacing="4" mt="8" align="stretch">
               <Link display="flex">
                 <Icon as={RiDashboardLine} fontSize="20" />
-                <Text ml="4" fontWeight="medium">Dashboard</Text>
+                <Text ml="4" fontWeight="medium">Pedidos</Text>
               </Link>
               <Link display="flex">
                 <Icon as={RiContactsLine} fontSize="20" />
-                <Text ml="4" fontWeight="medium">Usuarios</Text>
+                <Text ml="4" fontWeight="medium">Clientes</Text>
               </Link>
             </Stack>
           </Box>
           <Box>
-            <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
+            <Text fontWeight="bold" color="gray.400" fontSize="small">PRODUÇÃO</Text>
             <Stack spacing="4" mt="8" align="stretch">
               <Link display="flex">
                 <Icon as={RiInputMethodLine} fontSize="20" />
-                <Text ml="4" fontWeight="medium">Formularios</Text>
+                <Text ml="4" fontWeight="medium">Quadro de Produção</Text>
               </Link>
               <Link display="flex">
                 <Icon as={RiGitMergeLine} fontSize="20" />
-                <Text ml="4" fontWeight="medium">Automação</Text>
+                <Text ml="4" fontWeight="medium">Finalizados</Text>
               </Link>
             </Stack>
           </Box>
+
+          <Box>
+            <Text fontWeight="bold" color="gray.400" fontSize="small">FINANCEIRO</Text>
+            <Stack spacing="4" mt="8" align="stretch">
+              <Link display="flex">
+                <Icon as={RiInputMethodLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Lançamentos</Text>
+              </Link>
+              <Link display="flex">
+                <Icon as={RiGitMergeLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Fornecedores</Text>
+              </Link>
+            </Stack>
+          </Box>
+          
 
         </Stack>
 
